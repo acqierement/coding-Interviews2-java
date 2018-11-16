8fb8977645a2dfc1f736bfdc2655d3c98bdbc758
acqierement <xu@DESKTOP-EDK9F14>
2018-11-16 22:46:39 +0800

    updata 06

---
+++ src/example/NO_06.java
@@ -0,0 +1,44 @@
+package example;
+
+import java.util.Stack;
+/**
+ * 从尾到头打印链表的值
+ * @author acqierement
+ * Data: 2018年11月16日
+ * Time: 下午7:47:46
+ */
+public class NO_06 {
+	/*
+	 * 用栈的方法实现，从尾到头打印链表的值
+	 */
+	public static void printListReversingly_Iteratively(ListNode listNode) {
+		Stack<Integer> stack = new Stack<>();
+		while (listNode != null) {
+			stack.push(listNode.val);
+			listNode = listNode.next;
+		}
+
+		while(!stack.isEmpty()) {		
+			System.out.print(stack.pop() + " ");
+		}
+	}
+	
+	/*
+	 * 用递归的方法实现，从尾到头打印链表的值
+	 */
+	public static void printListReversingly_Recursively(ListNode listNode) {
+		if(listNode != null) {
+			printListReversingly_Recursively(listNode.next);
+			System.out.print(listNode.val + " ");
+		}
+	}
+	public static void main(String[] args) {
+		int[] array = {1,2,3,4,5};
+		ListNode root = ListUtil.contruct(array);
+		
+		printListReversingly_Iteratively(root);
+		System.out.println();
+		printListReversingly_Recursively(root);
+	}
+
+}
--- src/listNode/ListNode.java
+++
@@ -1,7 +0,0 @@
-package listNode;
-
- public class ListNode {
-     int val;
-     ListNode next;
-     ListNode(int x) { this.val = x; }
- }
