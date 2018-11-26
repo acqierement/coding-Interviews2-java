a0d15b1d72c0658f732ccfa7b9f5feaba3c88edf
acqierement <337048545@qq.com>
2018-11-26 17:35:47 +0800

    add NO_25

---
+++ src/problems/NO_25.java
@@ -0,0 +1,32 @@
+package problems;
+
+/**
+ * 25.合并两个排序的链表
+ * @author acqierement
+ * Data: 2018年11月26日
+ * Time: 下午5:23:08
+ */
+public class NO_25 {
+	public ListNode merge(ListNode l1, ListNode l2) {
+		ListNode head = new ListNode(0);
+		ListNode cur = head;
+		while(l1 != null && l2 != null) {
+			if(l1.val <= l2.val) {
+				cur.next = l1;
+				l1 = l1.next;
+			}else {
+				cur.next = l2;
+				l2 = l2.next;
+			}
+			cur = cur.next;//别忘了这一行
+		}
+		
+		if(l1 == null) {
+			cur.next = l2;
+		}
+		if(l2 == null) {
+			cur.next = l1;
+		}
+		return head.next;
+	}
+}
