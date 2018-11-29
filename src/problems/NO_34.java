5e141c4cc23277aefd2eea146a024f8f9493f80f
acqierement <337048545@qq.com>
2018-11-29 09:05:26 +0800

    add NO_34

---
+++ src/problems/NO_34.java
@@ -0,0 +1,33 @@
+package problems;
+
+import java.util.ArrayList;
+
+/**
+ * 34.二叉树中和为某一个值的路径
+ * @author acqierement
+ * Data: 2018年11月28日
+ * Time: 下午3:04:20
+ */
+public class NO_34 {
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int target) {
+    	ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+    	ArrayList<Integer> curRes = new ArrayList<>();
+    	FindPath(root, target, res, curRes);
+    	return res;
+    }
+
+	private void FindPath(TreeNode root, int target, ArrayList<ArrayList<Integer>> res,
+			ArrayList<Integer> curRes) {
+		if(root == null) {
+			return;
+		}
+		curRes.add(root.val);
+		if(target == root.val && root.left == null && root.right == null) {
+			res.add(new ArrayList<>(curRes));// 注意这里，别像我不注意把curRes直接放进去。
+		}
+		FindPath(root.left, target - root.val, res, curRes);
+		FindPath(root.right, target - root.val, res, curRes);
+		curRes.remove(curRes.size()-1);
+	}
+    
+}
