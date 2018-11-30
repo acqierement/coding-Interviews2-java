382e769c4b49ab65d200206f0b4c75e8b1228313
acqierement <337048545@qq.com>
2018-11-30 11:27:32 +0800

    add NO_42

---
+++ src/problems/NO_42.java
@@ -0,0 +1,40 @@
+package problems;
+
+/**
+ * 42.连续子数组的最大和
+ * LeetCode 53题
+ * @author acqierement
+ * Data: 2018年11月30日
+ * Time: 上午10:33:14
+ */
+public class NO_42 {
+	// 简单实现
+    public int maxSubArray(int[] nums) {
+    	if(nums == null || nums.length <= 0) {
+    		return 0;
+    	}
+    		
+        int curSum =nums[0];
+        int max = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+        	curSum = Math.max(curSum + nums[i], nums[i]);
+        	max = Math.max(max, curSum);
+        }
+        return max;
+    }
+    
+    // 动态规划
+    public int maxSubArrayDP(int[] nums) {
+    	if(nums == null || nums.length <= 0) {
+    		return 0;
+    	}
+    	int[] dp = new int[nums.length];
+    	dp[0] = nums[0];
+    	int max = nums[0];
+    	for(int i = 1; i <nums.length; i++) {
+    		dp[i] =dp[i-1] > 0 ? dp[i-1]+nums[i] : nums[i];
+    		max = Math.max(dp[i], max);
+    	}
+    	return max;
+    }
+}
