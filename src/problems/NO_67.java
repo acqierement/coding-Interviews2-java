77d3d7ceb4d9560873ce717bb2c141a581b3021d
acqierement <337048545@qq.com>
2018-12-06 20:12:31 +0800

    add NO_67

---
+++ src/problems/NO_67.java
@@ -0,0 +1,57 @@
+package problems;
+
+/**
+ * 67.把字符串转换成整数
+ * @author acqierement
+ * Data: 2018年12月6日
+ * Time: 下午8:11:55
+ */
+public class NO_67 {
+    public int StrToInt(String str) {
+        if(str == null || str.length() <= 0){
+            return 0;
+        }
+        int sign = 1, index = 0;
+        long sum = 0;
+        // 跳过空格
+        while(index < str.length() && str.charAt(index) == ' ') {
+        	index++;
+        }
+        if(index >= str.length()){
+            return 0;
+        }
+        // 第一个字符
+        if(str.charAt(index) == '-') {
+        	sign = -1;
+        	index++;
+        }else if(str.charAt(index) == '+') {
+//        	sign = 1;
+        	index++;
+        }
+        for(int i = index; i < str.length(); i++) {
+        	char c = str.charAt(i);
+        	if(c <= '9' && c >= '0') {
+        		sum = sum * 10 + (c - '0');
+        	}else {
+        		return 0;
+        	}
+        	
+           	if(sign * sum > Integer.MAX_VALUE) {
+        		sum = Integer.MIN_VALUE;
+        		break;
+        	}
+        	if(sign * sum < Integer.MIN_VALUE) {
+        		sum = Integer.MIN_VALUE;
+        		break;
+        	}
+//        	我们的sum是long类型的，所以可以用前面的方法判断溢出，如果用int可以用下面的这个判断
+//			if (sum > Integer.MAX_VALUE || (sum == Integer.MAX_VALUE / 10 && (c - '0') > 7)) {
+//				if (sign == 1)
+//					return Integer.MAX_VALUE;
+//				else
+//					return Integer.MIN_VALUE;
+//			}
+        }       
+        return (int) (sum * sign);
+    }
+}
