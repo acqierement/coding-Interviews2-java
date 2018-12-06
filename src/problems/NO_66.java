b0accd2ae4ceeb38f3c16a98ea7121b5e979d5aa
acqierement <337048545@qq.com>
2018-12-06 16:42:39 +0800

    add NO_66

---
+++ src/problems/NO_66.java
@@ -0,0 +1,28 @@
+package problems;
+
+
+/**
+ * 66.构建乘积数组
+ * @author acqierement
+ * Data: 2018年12月6日
+ * Time: 下午4:31:36
+ */
+public class NO_66 {
+	// 可以看一下书上那么二维数组的图，这里是通过对角线（灰色的1）来把矩阵划分成两个三角形。
+	// 整个过程看着书上那个二维矩阵会清楚很多
+    public int[] multiply(int[] A) {
+    	int[] B = new int[A.length];
+    	B[0] = 1;
+    	// 构建左下部分的那个三角形。
+    	for(int i = 1; i < B.length; i++) {
+    		B[i] = A[i - 1] * B[i - 1];
+    	}
+    	// 这里就没必要单独构建右上部分的三角形了，直接得到答案
+    	int temp = 1;
+    	for(int i = B.length - 2; i >= 0; i--) {
+    		temp *= A[i + 1];
+    		B[i] *= temp;
+    	}
+    	return B;
+    }
+}
